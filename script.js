aab7ddb310ec976a7cbc473bbda5b69ebebf0e0e
ScreamingInTheToilet <121836498+ScreamingInTheToilet@users.noreply.github.com>
2025-07-21 17:12:56 +0930

    Update script.js
    Added toggleMenu() function to toggle burger menu

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ document.querySelectorAll('.add-to-cart').forEach(button => {
     });
 });
 
+/*This function toggles the burger menu by creating a class attribute*/
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  /*
+  Creates constant variable menu using the element with ID "menu"
+  from the document.
+  */
+  menu.classList.toggle('show');
+  /*Create class attribute "show" for element with ID "menu"*/
+}
+
+
 // Mobile menu toggle (optional)
 // You can expand this for mobile responsiveness
